79bdca038052e2725e3fc0b59468027d1819acd8
Piotr Tarasewicz <piotrktarasewicz@gmail.com>
2026-02-23 15:36:13 +0100

    Update main.py
    jestem głupi!

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+import os
+import psycopg
 from fastapi import FastAPI
 
 app = FastAPI()
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 @app.get("/")
 def root():
     return {"status": "ok"}
+
+@app.get("/db-test")
+def db_test():
+    try:
+        with psycopg.connect(DATABASE_URL) as conn:
+            with conn.cursor() as cur:
+                cur.execute("SELECT 1;")
+                result = cur.fetchone()
+        return {"db": "connected", "result": result[0]}
+    except Exception as e:
+        return {"db": "error", "details": str(e)}
+
